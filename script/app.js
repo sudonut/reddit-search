70bd38a171382ba7f840d10be7c7869833b05fb5
sudonut <paradoxotl@gmail.com>
2021-07-06 20:25:56 -0400

    Created function to load images from reddit one at a time

--- script/app.js
+++ script/app.js
@@ -1,5 +1,8 @@
 document.getElementById("submit-search").addEventListener("click", fetchSubreddit);
 
+
+let i = 0;
+
 async function fetchSubreddit(e) {
   e.preventDefault();
   let subreddit = document.getElementById("sub-input").value;
@@ -9,11 +12,9 @@ async function fetchSubreddit(e) {
   let posts = subData.data.children
   console.log(posts);
 
-  let img = new Image();
-
   function createPost() {
-    for (let i = 0; i < 1; i++) {
-
+    
+    let img = new Image();
     img.src = posts[i].data["url_overridden_by_dest"];
     console.log(posts[i].data["url_overridden_by_dest"]);
 
@@ -27,27 +28,7 @@ async function fetchSubreddit(e) {
     let newThumbnail = document.createElement("div");
     newThumbnail.className = "thumbnail";
     newDiv.appendChild(img);
-    }
+    i++;
   }
   createPost();
 }
-
-// document.getElementById("thumbnail").append(img);
-
-// console.log(post.data["url_overridden_by_dest"])
-// let img = new Image();
-// img.src = subData.data.children[4].data["url_overridden_by_dest"]
-// document.getElementById("thumbnail").append(img);
-
-// posts.map(post => {
-//   // Creates a new wrapper div that will hold the retrieved information
-//   let newDiv = document.createElement("div");
-//   newDiv.className = "results-wrap";
-//   postsContainer.appendChild(newDiv);
-
-//   let newThumbnail = document.createElement("div");
-//   newThumbnail.className = "thumbnail";
-//   newDiv.appendChild(newThumbnail);
-  
-//   console.log(post.data["url_overridden_by_dest"])
-//   })
