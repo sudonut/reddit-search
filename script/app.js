533cf41c42fb4b3c0b0a984e62da4308d43a6b86
sudonut <paradoxotl@gmail.com>
2021-07-08 17:12:24 -0400

    Styled the search input and completed base subreddit search functions

--- script/app.js
+++ script/app.js
@@ -1,34 +1,31 @@
 document.getElementById("submit-search").addEventListener("click", fetchSubreddit);
 
-
-let i = 0;
-
 async function fetchSubreddit(e) {
   e.preventDefault();
   let subreddit = document.getElementById("sub-input").value;
   const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
   const subData = await response.json();
-
   let posts = subData.data.children
   console.log(posts);
 
   function createPost() {
-    
-    let img = new Image();
-    img.src = posts[i].data["url_overridden_by_dest"];
-    console.log(posts[i].data["url_overridden_by_dest"]);
-
     let postsContainer = document.getElementById("posts-container");
+    // Create a new div for each image
+    for (let i = 0; i < posts.length; i++) {
+      let img = new Image();
+      img.src = posts[i].data["url_overridden_by_dest"];
 
-    // Creates a new wrapper div that will hold the retrieved information
-    let newDiv = document.createElement("div");
-    newDiv.className = "results-wrap";
-    postsContainer.appendChild(newDiv);
-
-    let newThumbnail = document.createElement("div");
-    newThumbnail.className = "thumbnail";
-    newDiv.appendChild(img);
-    i++;
+      let newDiv = document.createElement("div");
+      newDiv.className = "results-wrap";
+      postsContainer.appendChild(newDiv);
+  
+      let newImg = document.createElement("div");
+      newImg.className = "thumbnail";
+      newDiv.appendChild(img)
+    }
   }
   createPost();
 }
+
+//Would this make sense to someone else?
+
