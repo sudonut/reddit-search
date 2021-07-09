396b16b492425e2ad53e6e1b4f86e371c6dbbc52
sudonut <paradoxotl@gmail.com>
2021-07-09 15:02:20 -0400

    Placed the createPost function outside of the fetchSubreddit function

--- script/app.js
+++ script/app.js
@@ -8,31 +8,28 @@ async function fetchSubreddit(e) {
   const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
   const subData = await response.json();
   const posts = subData.data.children
-  console.log(posts);
-
-  function createPost() {
-    // Create a new div for each image
-    for (let i = 0; i < posts.length; i++) {
-      let img = new Image();
-      img.src = posts[i].data["url_overridden_by_dest"];
-
-      let newDiv = document.createElement("div");
-      newDiv.className = "results-wrap";
-      postsContainer.appendChild(newDiv);
-  
-      let newImg = document.createElement("div");
-      newImg.className = "thumbnail";
-      newDiv.appendChild(img)
-    }
-  }
-  createPost();
+  createPost(posts);
 }
+
+function createPost(posts) {
+  // Create a new div for each image
+  for (let i = 0; i < posts.length; i++) {
+    let img = new Image();
+    img.src = posts[i].data["url_overridden_by_dest"];
+
+    let newDiv = document.createElement("div");
+    newDiv.className = "results-wrap";
+    postsContainer.appendChild(newDiv);
+
+    let newImg = document.createElement("div");
+    newImg.className = "thumbnail";
+    newDiv.appendChild(img)
+  }
+};
 
 postsContainer.addEventListener("scroll", async (e) => {
   if (postsContainer.scrollTop + postsContainer.clientHeight >= postsContainer.scrollHeight) {
     fetchSubreddit(e);
     console.log("Est");
-  }
-})
-//Would this make sense to someone else?
-
+  };
+});
