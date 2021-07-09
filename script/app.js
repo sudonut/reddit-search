aea8f2e67a95932b5a551c7c68fa43ca96a07af7
sudonut <paradoxotl@gmail.com>
2021-07-09 19:39:10 -0400

    Added scrolling function to retrieve more posts.

--- script/app.js
+++ script/app.js
@@ -1,5 +1,4 @@
 document.getElementById("submit-search").addEventListener("click", fetchPosts);
-
 const postsContainer = document.getElementById("posts-container");
 
 async function fetchPosts(e) {
@@ -7,20 +6,21 @@ async function fetchPosts(e) {
   const subreddit = document.getElementById("sub-input").value;
   let response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25&after=`);
   let subData = await response.json();
-  let afterVal = subData.data.after;
-  let posts = subData.data.children
-  console.log(posts);
-  if (afterVal !== undefined) {
-    response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25&after=${afterVal}`)
+  let posts = subData.data.children;
+  function fetchScroll() {
+    postsContainer.addEventListener("scroll", async () => {
+      if (postsContainer.scrollTop + postsContainer.clientHeight >= postsContainer.scrollHeight) {
+        afterVal = subData.data.after;
+        response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25&after=${afterVal}`)
+        subData = await response.json();
+        posts = subData.data.children;
+        createPost(posts);
+      };
+    });
   }
+  fetchScroll();
   createPost(posts);
 };
-
-// async function fetchMorePosts(subData) {
-//   subData = await response.json();
-//   posts = subData.data.children;
-//   createPost(posts);
-// }
 
 function createPost(posts) {
   // Create a new div for each image
@@ -37,14 +37,3 @@ function createPost(posts) {
     newDiv.appendChild(img)
   };
 };
-
-postsContainer.addEventListener("scroll", async (e) => {
-  if (postsContainer.scrollTop + postsContainer.clientHeight >= postsContainer.scrollHeight) {
-    fetchPosts(e);
-    console.log("Est");
-  };
-});
-
-
-
-// https://www.reddit.com/r/redditdev/comments/d7egb/how_to_get_more_json_results_i_get_only_30/
