a2fd3b46730518665fd2a827711180db504f83b0
sudonut <paradoxotl@gmail.com>
2021-07-09 17:52:25 -0400

    Changed the name of some functions

--- script/app.js
+++ script/app.js
@@ -1,15 +1,26 @@
-document.getElementById("submit-search").addEventListener("click", fetchSubreddit);
+document.getElementById("submit-search").addEventListener("click", fetchPosts);
 
 const postsContainer = document.getElementById("posts-container");
 
-async function fetchSubreddit(e) {
+async function fetchPosts(e) {
   e.preventDefault();
   const subreddit = document.getElementById("sub-input").value;
-  const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
-  const subData = await response.json();
-  const posts = subData.data.children
+  let response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25&after=`);
+  let subData = await response.json();
+  let afterVal = subData.data.after;
+  let posts = subData.data.children
+  console.log(posts);
+  if (afterVal !== undefined) {
+    response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25&after=${afterVal}`)
+  }
   createPost(posts);
-}
+};
+
+// async function fetchMorePosts(subData) {
+//   subData = await response.json();
+//   posts = subData.data.children;
+//   createPost(posts);
+// }
 
 function createPost(posts) {
   // Create a new div for each image
@@ -24,12 +35,16 @@ function createPost(posts) {
     let newImg = document.createElement("div");
     newImg.className = "thumbnail";
     newDiv.appendChild(img)
-  }
+  };
 };
 
 postsContainer.addEventListener("scroll", async (e) => {
   if (postsContainer.scrollTop + postsContainer.clientHeight >= postsContainer.scrollHeight) {
-    fetchSubreddit(e);
+    fetchPosts(e);
     console.log("Est");
   };
 });
+
+
+
+// https://www.reddit.com/r/redditdev/comments/d7egb/how_to_get_more_json_results_i_get_only_30/
