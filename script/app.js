d3844ad526c768120a34056c11492d487ed2ec6d
sudonut <paradoxotl@gmail.com>
2021-07-12 16:33:10 -0400

    Fixed a bug where upon searching for a new subreddit, images from the previous searches would show up as well.

--- script/app.js
+++ script/app.js
@@ -4,26 +4,23 @@ const postsContainer = document.getElementById("posts-container");
 async function fetchPosts(e) {
   e.preventDefault();
   const subreddit = document.getElementById("sub-input").value;
-  let response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25&after=`);
+  let response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25`);
   let subData = await response.json();
   let posts = subData.data.children;
-  function fetchScroll() {
-    postsContainer.addEventListener("scroll", async () => {
-      if (postsContainer.scrollTop + postsContainer.clientHeight >= postsContainer.scrollHeight) {
-        afterVal = subData.data.after;
-        response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25&after=${afterVal}`)
-        subData = await response.json();
-        posts = subData.data.children;
-        createPost(posts);
-      };
-    });
+  afterVal = subData.data.after;
+  // Prevents duplicate posts
+  if (posts && posts.length > 0) {
+    lastId = posts[posts.length - 1].data.id;
+  } else {
+    lastId = undefined;
   }
-  fetchScroll();
+  clearPosts(postsContainer);
   createPost(posts);
+  fetchScroll(afterVal);
 };
 
+// Create a new div for each image
 function createPost(posts) {
-  // Create a new div for each image
   for (let i = 0; i < posts.length; i++) {
     let img = new Image();
     img.src = posts[i].data["url_overridden_by_dest"];
@@ -37,3 +34,22 @@ function createPost(posts) {
     newDiv.appendChild(img)
   };
 };
+
+function clearPosts(postsContainer) {
+  while (postsContainer.firstChild) {
+    postsContainer.removeChild(postsContainer.firstChild);
+  }
+}
+
+function fetchScroll(afterVal) {
+  postsContainer.addEventListener("scroll", async () => {
+    const subreddit = document.getElementById("sub-input").value;
+    if (postsContainer.scrollTop + postsContainer.clientHeight >= postsContainer.scrollHeight) {
+      let response = await fetch(`https://www.reddit.com/r/${subreddit}.json?count=25&after=${afterVal}`)
+      let subData = await response.json();
+      afterVal = subData.data.after;
+      let posts = subData.data.children;
+      createPost(posts);
+    };
+  });
+}
