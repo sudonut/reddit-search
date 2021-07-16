38d695ff5bbd9e68d360e07ac1c0c5849b802262
sudonut <paradoxotl@gmail.com>
2021-07-15 20:07:52 -0400

    Changed html layout, created function to generate pictures to new containers

--- script/app.js
+++ script/app.js
@@ -3,6 +3,10 @@ const postsContainer = document.getElementById("posts-container");
 const loader = document.querySelector(".load-container");
 const inputField = document.getElementById("sub-input");
 
+let column1 = document.getElementById("vertical-container1"),
+    column2 = document.getElementById("vertical-container2"),
+    column3 = document.getElementById("vertical-container3");
+
 inputField.addEventListener("change", () => {
   nextPageId = undefined;
   clearPosts(postsContainer);
@@ -35,6 +39,7 @@ async function fetchPosts(e) {
   const posts = data.data.children;
   nextPageId = data.data.after;
 
+  console.log(posts)
   // Prevents duplicate posts
   if (posts && posts.length > 0) {
     lastId = posts[posts.length - 1].data.id;
@@ -42,10 +47,12 @@ async function fetchPosts(e) {
     lastId = undefined;
   }
 
-  createPost(posts);
+  sendToContainers(posts)
+  // createPost(posts);
   isFetching = false;
   loader.classList.remove("active");
 };
+
 
 // Create a new div for each image
 function createPost(posts) {
@@ -57,7 +64,7 @@ function createPost(posts) {
 
     let newDiv = document.createElement("div");
     newDiv.className = "results-wrap";
-    postsContainer.appendChild(newDiv);
+    column1.appendChild(newDiv);
 
     let newImg = document.createElement("div");
     newImg.className = "thumbnail";
@@ -68,6 +75,57 @@ function createPost(posts) {
     }, 500)
   };
 };
+
+function createPost1(posts) {
+  for (let i = 0; i < posts.length; i++) {
+    loader.classList.add("active");
+
+    let img = new Image();
+    img.src = posts[i].data["url_overridden_by_dest"];
+
+    let newDiv = document.createElement("div");
+    newDiv.className = "results-wrap";
+    column2.appendChild(newDiv);
+
+    let newImg = document.createElement("div");
+    newImg.className = "thumbnail";
+    newDiv.appendChild(img);
+    
+    setTimeout(() => {
+      loader.classList.remove("active");
+    }, 500)
+  };
+};
+
+function createPost2(posts) {
+  for (let i = 0; i < posts.length; i++) {
+    loader.classList.add("active");
+
+    let img = new Image();
+    img.src = posts[i].data["url_overridden_by_dest"];
+
+    let newDiv = document.createElement("div");
+    newDiv.className = "results-wrap";
+    column3.appendChild(newDiv);
+
+    let newImg = document.createElement("div");
+    newImg.className = "thumbnail";
+    newDiv.appendChild(img);
+    
+    setTimeout(() => {
+      loader.classList.remove("active");
+    }, 500)
+  };
+};
+// constructor function? Class? loop through the element classes selector??
+function sendToContainers(posts) {
+  col1 = posts.slice(0, 9);
+  createPost(col1)
+  col2 = posts.slice(9, 18);
+  createPost1(col2)
+  col3 = posts.slice(18, 27);
+  createPost2(col3)
+}
 
 postsContainer.addEventListener("scroll", async (e) => {
   // Do not run if currently fetching data
